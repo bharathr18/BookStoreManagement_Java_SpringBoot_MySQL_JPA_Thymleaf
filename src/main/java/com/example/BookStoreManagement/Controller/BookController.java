99266a58c93b03e6d71481f71dd1a6042ad19b6e
bharathr18 <bharathr.16ec@saividya.ac.in>
2023-04-14 03:20:01 +0530

    Initial commit

---
+++ src/main/java/com/example/BookStoreManagement/Controller/BookController.java
@@ -0,0 +1,16 @@
+package com.example.BookStoreManagement.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+public class BookController {
+
+    @GetMapping("/")
+    public String home()
+    {
+        return "home";
+    }
+}
